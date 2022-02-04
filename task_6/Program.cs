507c44cd4096195c0a46cc64f70f55c50938d54d
LarisaZaripova <larik7lolik@gmail.com>
2022-02-04 15:13:19 +0300

    added task_6

---
+++ task_6/Program.cs
@@ -0,0 +1,16 @@
+﻿//Задача 6: Выяснить является ли число чётным
+
+Console.Write("Введите число: ");
+
+int num = int.Parse(Console.ReadLine());
+
+int n = num / 2;
+
+if(num / 2);
+{
+   Console.WriteLine(" Число " + num + " чётное ");
+}
+else
+{
+    Console.WriteLine(" Число " + num + " нечётное ");
+}
