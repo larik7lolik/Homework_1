b8e52bc3bf023afe52035dab19945d001a6332e5
LarisaZaripova <larik7lolik@gmail.com>
2022-02-04 13:47:25 +0300

    added task_4

---
+++ task_4/Program.cs
@@ -0,0 +1,14 @@
+﻿//Задача 4: Найти максимальное из трёх чисел.
+
+int a = 1220;
+int b = 25;
+int c = 63;
+int max = a;
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+
+    Console.Write("max = ");
+    Console.WriteLine(max);
+
+
