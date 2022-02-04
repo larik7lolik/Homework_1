bd50b9ddfc8249fe139dee785f65529ed8776896
LarisaZaripova <larik7lolik@gmail.com>
2022-02-04 16:12:05 +0300

    added task_8

---
+++ task_8/Program.cs
@@ -0,0 +1,11 @@
+﻿//Задача 8: Показать чётные числа от 1 до N.
+
+Console.Write("Введите число: ");
+
+int N = int.Parse(Console.ReadLine());
+int i = 2;
+while(i <= N)
+{
+    Console.WriteLine( i );
+    i+=2;
+}
