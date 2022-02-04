f4fe98a5dc60749453b394991676afb4752a26c2
LarisaZaripova <larik7lolik@gmail.com>
2022-02-04 13:03:36 +0300

    added task_2

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿//Задача 2: Даны два числа. Показать большее и меньшее число
+
+int numberA = new Random().Next(1, 10); // 1 2 3 4 ...9
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1, 10);
+Console.WriteLine(numberB);
+
+if(numberA > numberB);
+{
+    Console.WriteLine(numberA  +  " Большее число ");
+    Console.WriteLine(numberB  +  " Меньшее число ");
+}
+ //else if (numberA < numberB);
+//{
+   // Console.WriteLine(numberB  +  "Большее число");
+    //Console.WriteLine(numberB  +  "Меньшее число");
+//}
+//else 
+//{
+   // Console.WriteLine(" Числа равны ");
+//}
